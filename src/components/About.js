ea7b0784522414f5e66e1f5c3052fc7899077afa
thejas <thejaskala308@gmail.com>
2022-07-02 21:11:02 +0530

    male

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About(){
                 <center>
                 	<h1 className='ui header' style={{padding:'10px'}} >About Us</h1>
                 </center>
-                <p style={{fontSize:'20px'}}>Simple Single Page Web App , Nothing To Tell Goddu Bye...</p>
+                <p style={{fontSize:'20px',color:'white'}}>Simple Single Page Web App , Nothing To Tell Goddu Bye...</p>
             </div></div>
         
     )
