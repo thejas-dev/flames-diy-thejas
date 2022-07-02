4b446bf270fca748e12f0e6f93f15908582edb8a
thejas <thejaskala308@gmail.com>
2022-07-02 22:43:15 +0530

    maske it

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About(){
                 <center>
                 	<h1 className='ui header' style={{padding:'10px'}} >About Us</h1>
                 </center>
-                <p style={{fontSize:'20px',color:'white',textShadow:'2px 2px 4px #000000'}}><b>Simple Single Page Flames Game Site , Nothing is Real In This Creul World...just Read Again, Goddu Bye...</b></p>
+                <p style={{fontSize:'20px',color:'white',textShadow:'2px 2px 4px #000000'}}><b>A Fun Filled Game Site...Check Your Relationship Between Yr Loved Ones... Have Fun...</b></p>
             </div></div>
         
     )
