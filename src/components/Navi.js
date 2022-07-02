35096a1a27d91303522812ee95e8d65277fa79ce
thejas <thejaskala308@gmail.com>
2022-07-02 22:03:03 +0530

    make me

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -4,7 +4,7 @@ function Navi(){
     return(
         <nav className="box" style={{height:'100px', padding:'5px', borderRadius : '20px 20px 0px 0px'}} >
             <center>
-        <h2 className="ui header" style={{padding:'15px',fontSize:'35px' ,background: 'linear-gradient(to right,#fcc133,#fc33d0)',
+        <h2 className="ui header" style={{padding:'15px',fontSize:'35px' ,background: 'linear-gradient(to right,#cc3d95,#d47db2)',
     WebkitBackgroundClip:'text',
     backgroundClip: 'text',
     color: 'transparent'}}>Flames Game</h2></center>
