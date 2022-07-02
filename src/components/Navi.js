74a7a15035bd9ad2ec4cd50ee53af5869fd08bfb
thejas <thejaskala308@gmail.com>
2022-07-02 21:34:50 +0530

    make it better

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -4,10 +4,10 @@ function Navi(){
     return(
         <nav className="box" style={{height:'100px', padding:'5px', borderRadius : '20px 20px 0px 0px'}} >
             <center>
-        <h2 className="ui header" style={{padding:'15px' ,background: 'linear-gradient(to right,#fcc133,#fc33d0)',
+        <h2 className="ui header" style={{padding:'15px',fontSize:'40px' ,background: 'linear-gradient(to right,#fcc133,#fc33d0)',
     WebkitBackgroundClip:'text',
     backgroundClip: 'text',
-    color: 'transparent'}}>Flames Calculator</h2></center>
+    color: 'transparent'}}>Flames Game</h2></center>
         <div className='ui three buttons'>
             <button className='ui button'><a href='/'>Flames</a></button>
             <button className='ui button'><a href='/about'>About Us</a></button>
